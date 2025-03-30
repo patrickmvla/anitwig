f5449d9582f27de2ef40af0ccd5fe7a0e58ea8f6
Patrick Mvula <mvlapatrick@gmail.com>
2025-03-30 02:24:48 +0200

    feat: auth config

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,15 @@
+import { db } from "@/db";
 import { betterAuth } from "better-auth";
- 
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import * as schema from "@/db/schema";
+
 export const auth = betterAuth({
-    //...
-})
+  database: drizzleAdapter(db, {
+    provider: "pg",
+    usePlural: true,
+    schema,
+  }),
+  emailAndPassword: {
+    enabled: true
+  }
+});
